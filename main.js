fb0c7a2c7f767d1a28586e49ec4c32a9365ac3b1
Marcos <r.rossimarcos@gmail.com>
2022-03-14 00:34:19 +0100

    Revert "git conflicts"

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-const saludar = () => {
-    return 'hola';
-}
+validation();
+console.log('Hola soy el 1er commit de main.js');
 
 function validation() {
     return true;
 }
-
-validation();
-console.log('Hola soy el 1er commit de main.js');
